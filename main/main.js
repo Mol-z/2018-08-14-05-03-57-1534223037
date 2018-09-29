3d6b6327a0ba16a59532437caed4d7ae9ba4f20c
zmy <zhangmengying@buaa.edu.cn>
2018-09-29 13:17:36 +0800

    changed main file

--- main/main.js
+++ main/main.js
@@ -1,3 +1,7 @@
-module.exports = function main() {
+module.exports = function main(str) {
   // Write your code here
+  var sum = null;
+  for(var i = 0; i < str.length; i++)
+     sum += str[i];
+  return sum;
 };
